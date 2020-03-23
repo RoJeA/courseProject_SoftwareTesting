504ec091fb0ae3d3d98bd648db3a5ff062ef0757
108598003 Rojean <t108598003@ntut.org.tw>
2020-03-23 16:28:25 +0800

    add GitHashTest(partly)

--- src/test/java/com/github/davidmoten/geo/GeoHashTest.java
+++ src/test/java/com/github/davidmoten/geo/GeoHashTest.java
@@ -4,49 +4,93 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class GeoHashTest {
-
+    String hash;
+    Direction direction;
+    GeoHash geoHash;
     @Before
     public void setUp() throws Exception {
+        hash = "11w";
+        direction = Direction.TOP;
     }
 
     @After
     public void tearDown() throws Exception {
+        hash = null;
+        direction = null;
     }
 
     @Test
     public void adjacentHash() {
+        String adjacentHash = geoHash.adjacentHash(hash, direction,-1);
+        assertEquals(geoHash.adjacentHash("11w", Direction.BOTTOM, 1), adjacentHash);
 
+        adjacentHash = geoHash.adjacentHash(hash, direction, 5);
+        assertEquals("14y", adjacentHash);
     }
 
     @Test
     public void right() {
+        assertEquals("11x", geoHash.right(hash));
     }
 
     @Test
     public void left() {
+        assertEquals("11t", geoHash.left(hash));
     }
 
     @Test
     public void top() {
+        assertEquals("11y", geoHash.top(hash));
     }
 
     @Test
     public void bottom() {
+        assertEquals("11q", geoHash.bottom(hash));
     }
 
     @Test
     public void testAdjacentHash() {
+        String adjacentHash = geoHash.adjacentHash(hash, direction);
+        assertEquals("11y",adjacentHash);
+
+        adjacentHash = geoHash.adjacentHash("zzz", Direction.TOP);
+        assertEquals("gzz",adjacentHash);
+
+        adjacentHash = geoHash.adjacentHash("145",Direction.BOTTOM);
+        assertEquals("11g",adjacentHash);
+
+        adjacentHash = geoHash.adjacentHash("000",Direction.LEFT);
+        assertEquals("pbp",adjacentHash);
+
+        adjacentHash = geoHash.adjacentHash("ppp",Direction.RIGHT);
+        assertEquals("pr0",adjacentHash);
     }
 
     @Test
     public void neighbours() {
+        List<String> neighbours = new ArrayList<String>();
+        neighbours = geoHash.neighbours(hash);
+        List<String> compare = new ArrayList<String>();
+        compare.add("11t"); //left
+        compare.add("11x"); //right
+        compare.add("11y"); //top
+        compare.add("11q"); //bottom
+        compare.add("11v"); //left-top
+        compare.add("11m"); //left-bottom
+        compare.add("11z"); //right-top
+        compare.add("11r"); //right-bottom
+        assertEquals(compare, neighbours);
     }
 
     @Test
     public void encodeHash() {
+
     }
 
     @Test
